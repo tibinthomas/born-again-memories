a8fe7804476ebc85b03f50991aa5df4f92c872b1
Tibin Thomas <tibinthomas@outlook.com>
2026-05-03 21:15:52 +0530

    Refactor package names and update bundle identifier for consistency

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.born_again_memories"
+    namespace = "com.tibinthomas.born_again_memories"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -22,7 +22,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.born_again_memories"
+        applicationId = "com.tibinthomas.born_again_memories"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- android/app/src/main/kotlin/com/tibinthomas/born_again_memories/MainActivity.kt
+++ android/app/src/main/kotlin/com/tibinthomas/born_again_memories/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.born_again_memories
+package com.tibinthomas.born_again_memories
 
 import io.flutter.embedding.android.FlutterActivity
 
